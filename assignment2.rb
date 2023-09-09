06ab9e19306efa50a19f079960d068fb95a71822
FashionableOtter <benj.seaton@gmail.com>
2023-09-09 13:53:27 +1000

    Add assignment2.rb
    Added Ruby code file assignment2.rb

---
+++ assignment2.rb
@@ -0,0 +1,23 @@
+def split_and_sort(nums)
+  raise "Error: Input list should not contain more than 20 integers." if nums.length > 20
+  raise "Error: The number 0 is not a valid input." if nums.include?(0)
+
+  odd_nums = nums.select { |num| num.odd? }.sort
+  even_nums = nums.select { |num| num.even? }.sort
+
+  [odd_nums, even_nums]
+end
+
+nums = [5, 46, 31, 34, 9, 4, 5, 29, 31, 31, 46, 7, 4, 29, 34, 5, 8, 23, 56, 6]
+for i in 0..nums.length do
+  for j in 0..nums.length do
+    if i != j
+      if nums[i] == nums[j]
+        nums.delete_at(j)
+      end
+    end
+  end
+end
+odd_nums, even_nums = split_and_sort(nums)
+puts "Odd numbers: #{odd_nums}"
+puts "Even numbers: #{even_nums}"
